650bec50483f440a1257af20c5f97d3a941ac8cc
Ibahm <ibrahimahm92@gmail.com>
2022-08-16 00:36:31 +0100

    Added about me section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Brief from "./components/Brief";
+import About from "./components/About";
 
 const App = () => {
   return (
     <div className="container">
       <NavBar/>
       <Brief/>
+      <About/>
     </div>
   )
 }
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <div className="about-text">
+                <h1 className="about-name">About Me</h1>
+                <p className="about-desc1">I am a software enthusiast that loves to learn topics and displaying my newly found skills through projects.
+                 My love for programming began from my very first website project in 2014 where I discovered the intricacy of <span>CSS</span>. Wanting to learn how to improve UI elements 
+                 outside of CSS' reach, I discovered JavaScript.</p>
+                 <p className="about-desc2">Throughout my time at university, I had taken part in a multitude of projects demonstrating my understanding of <span>Java</span> through projects including
+                 <span> traversing a Finch Robot</span> and <span>a login and register page via an Android application</span> a login and register page via an Android application but through personal projects and online tests, I was able to maintain a growing 
+                 understanding of <span>JavaScript</span>. This shows that I am always in pursuit of a challenge.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
