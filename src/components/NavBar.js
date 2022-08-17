38b3bb839380c542a0cd6a9d450d980a19d7afaa
Ibahm <ibrahimahm92@gmail.com>
2022-08-17 23:15:46 +0100

    Added links to the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,6 @@
 import React, {useEffect} from "react";
+import {Link} from "react-scroll";
+import About from "./About";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
@@ -10,10 +12,26 @@ const NavBar = () => {
     return (
         <div className="nav-container">
             <ul>
-                <li className="about" data-aos="fade-down" data-aos-duration="1000"><a href="#About">About</a></li>
-                <li className="projects" data-aos="fade-down" data-aos-duration="1200"><a href="#">Projects</a></li>
-                <li className="contact" data-aos="fade-down" data-aos-duration="1300"><a href="#">Contact</a></li>
-                <li><div data-aos="fade-down" data-aos-duration="1400"><button className="nav-resume">Resume</button></div></li>
+                <li className="about" data-aos="fade-down" data-aos-duration="1000">
+                    <Link to="about-container" spy={true} smooth={true} offset={-200} duration={500}>
+                        <p>About</p>
+                    </Link>    
+                </li>
+                <li className="projects" data-aos="fade-down" data-aos-duration="1200">
+                    <Link to="projectlist-container" spy={true} smooth={true} offset={-200} duration={500}>
+                        <p>Projects</p>
+                    </Link>
+                </li>
+                <li className="contact" data-aos="fade-down" data-aos-duration="1300">
+                    <Link to="contact-container" spy={true} smooth={true} offset={-200} duration={500}>
+                        <p>Contact</p>
+                    </Link>
+                </li>
+                <li>
+                    <div data-aos="fade-down" data-aos-duration="1400">
+                        <button className="nav-resume">CV</button>
+                    </div>
+                </li>
             </ul>
         </div>
     )
